7a959ae917b0a6e0ede9ec70c3ad72a117454d5a
Nojus R <42602537+had0pelagic@users.noreply.github.com>
2022-09-02 18:36:37 +0300

    Api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    ApiKey: process.env.REACT_APP_API_KEY,
+  },
+});
 
 const api = {
   post: async (path, data) => {
